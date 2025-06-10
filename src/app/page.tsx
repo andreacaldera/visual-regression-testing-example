ce342f6f1f8ebb13a3b0efb13b7397242edd4866
Andrea Caldera <andrea.caldera@gmail.com>
2025-06-10 15:47:35 +0100

    visual changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
-          width={300}
+          width={400}
           height={50}
           priority
         />
